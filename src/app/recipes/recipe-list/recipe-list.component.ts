af3a5e9d9630ac32a5a3f4b1d2ff5b2ecc575e41
rashmi <rashmi.manandhar7@gmail.com>
2018-01-21 19:32:20 -0500

    basic setup

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'this is a test', 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fimage%2Frecipes%2Fay%2F07%2Fparsnips-carrots-ay-1875791-x.jpg%3Fitok%3DBKhk_raT&w=700&q=85')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
